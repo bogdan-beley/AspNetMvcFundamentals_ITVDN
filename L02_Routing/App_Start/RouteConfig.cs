4192660078a86e5e5db60a1fbdfceb40ae2693d4
Bogdan Beley <bogdan.beley@outlook.com>
2020-06-08 15:09:47 +0300

    L02_Routing

---
+++ L02_Routing/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace L02_Routing
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = "DefaultId"}
+            );
+
+            routes.MapRoute(
+                name: "PublicRoute",
+                url: "public/{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index", id = "DefaultId"}
+            );
+
+            routes.MapRoute(
+                name: "SegmentVariables",
+                url: "{controller}/{action}/{x}/{y}",
+                defaults: new { controller = "Home", action = "Index", x = 0, y = 0}
+            );
+
+            routes.MapRoute(
+                name: "CatchAllSegments",
+                url: "{controller}/{action}/{id}/{*catchall}",
+                defaults: new { controller = "Home", action = "Index", x = 0, y = 0 }
+            );
+        }
+    }
+}
---
+++ L02_Routing/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L02_Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Variable = RouteData.Values["id"];
+            return View();
+        }
+
+        public ActionResult CustomVariable(int? id)
+        {
+            ViewBag.Variable = id;
+            return View("Index");
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult SegmentVariables(int? x, int? y)
+        {
+            ViewBag.Variable = x + y;
+            return View();
+        }
+
+        public ActionResult CatchAllSegments()
+        {
+            ViewBag.Id = RouteData.Values["Id"];
+            ViewBag.CatchAll = RouteData.Values["CatchAll"];
+
+            return View();
+        }
+
+        public ActionResult ActionLink()
+        {
+            return View();
+        }
+    }
+}
