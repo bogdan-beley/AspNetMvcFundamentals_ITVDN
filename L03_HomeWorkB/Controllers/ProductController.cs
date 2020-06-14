f3c73c25e22ddbbf590fbcb40f89a6cf27188012
Bogdan Beley <bogdan.beley@outlook.com>
2020-06-14 10:02:52 +0300

    L03_HomeWorkB

---
+++ L03_HomeWorkB/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using L03_HomeWorkB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L03_HomeWorkB.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            products = ProductCollection.All;
+
+            return View(products);
+        }
+        
+        [ChildActionOnly]
+        public ActionResult ProductTable(int numberOfRows = 5)
+        {
+            IEnumerable<Product> products = ProductCollection.All.Take(numberOfRows);
+            
+            return PartialView("_ProductTable", products);
+        }
+    }
+}
---
+++ L03_HomeWorkB/Models/ProductCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace L03_HomeWorkB.Models
+{
+    public class ProductCollection
+    {
+        public static List<Product> All
+        {
+            get
+            {
+                List<Product> products = new List<Product>();
+                for (int i = 0; i < 30; i++)
+                {
+                    products.Add(new Product()
+                    {
+                        Id = i + 1,
+                        Name = "Product" + " " + $"{i + 1}",
+                        Price = (i + 1) * 2,
+                        CreatedDate = DateTime.Now
+                    });
+                }
+                return products;
+            }
+        }
+    }
+}
