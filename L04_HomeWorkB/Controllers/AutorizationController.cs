5d79f2bfd00a2ef9a6976f9b8a2e441646acdae2
Bogdan Beley <bogdan.beley@outlook.com>
2020-06-18 20:57:23 +0300

    L04_HomeWorkB

---
+++ L04_HomeWorkB/Controllers/AutorizationController.cs
@@ -0,0 +1,64 @@
+﻿using L04_HomeWorkB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L04_HomeWorkB.Controllers
+{
+    public class AutorizationController : Controller
+    {
+        // GET: Autorization
+        public ActionResult Login()
+        {
+            List<Login> logins = new List<Login>();
+
+            logins.Add(new Login()
+            {
+                Username = TempData["username"] as string,
+                Password = TempData["password"] as string
+            });
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            
+
+            TempData["username"] = username;
+            TempData["password"] = password;
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            List<Registration> registrations = new List<Registration>();
+
+            registrations.Add(new Registration()
+            {
+                Username = TempData["username"] as string,
+                Password = TempData["password"] as string,
+                PassConfirm = TempData["passconfirm"] as string,
+                Email = TempData["email"] as string
+            });
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(string username, string password, string passconfirm, string email)
+        {
+            TempData["username"] = username;
+            TempData["password"] = password;
+            TempData["passconfirm"] = passconfirm;
+            TempData["email"] = email;
+           
+            return View();
+        }
+    }
+}
---
+++ L04_HomeWorkB/Models/Registration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L04_HomeWorkB.Models
+{
+    public class Registration
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PassConfirm { get; set; }
+        public string Email { get; set; }
+    }
+}
