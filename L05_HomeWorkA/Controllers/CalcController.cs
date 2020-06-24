a7a6de67dc082cd111bab78f2fb8f07d5bd574cf
Bogdan Beley <bogdan.beley@outlook.com>
2020-06-24 18:18:51 +0300

    L05_HomeWorkA

---
+++ L05_HomeWorkA/Controllers/CalcController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L05_HomeWorkA.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int number1, int number2, string calcMethod)
+        {
+            int result = 0;
+            switch (calcMethod)
+            {
+                case "+": result = number1 + number2;
+                    break;
+                case "-": result = number1 - number2;
+                    break;
+                case "*": result = number1 * number2;
+                    break;
+                case "/": result = number1 / number2;
+                    break;
+                default:
+                    break;
+            }
+
+            ViewBag.result = result;
+
+            return View();
+        }
+    }
+}
---
+++ L05_HomeWorkA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L05_HomeWorkA.MvcApplication" Language="C#" %>
