c357e657cdee7a643a5da7e169f3f4bc04fbc410
Bogdan Beley <bogdan.beley@outlook.com>
2020-07-02 21:20:33 +0300

    L06_HomeWorkB2

---
+++ L06_HomeWorkB2/Controllers/ReportsController.cs
@@ -0,0 +1,51 @@
+﻿using L06_HomeWorkB2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L06_HomeWorkB2.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SalesReport()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SalesReport(Report incommingData)
+
+        {
+            var orders = db.Orders.Where(o => o.PurchaseDate >= incommingData.FromDate
+            && o.PurchaseDate <= incommingData.ToDate);
+
+            return PartialView("_Orders", orders.ToList());
+        }
+
+        public ActionResult UsersReport()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UsersReport(Report incommingData)
+
+        {
+            var orders = db.Orders.Where(o =>
+                o.User.FirstName == incommingData.Search 
+                || o.User.LastName == incommingData.Search);
+
+            return PartialView("_Orders", orders.ToList());
+        }
+    }
+}
---
+++ L06_HomeWorkB2/Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using L06_HomeWorkB2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace L06_HomeWorkB2.Models
+{
+    public class Report
+    {
+        [DataType(DataType.Date)]
+        public DateTime FromDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ToDate { get; set; }
+
+        public string Search { get; set; }
+    }
+}
